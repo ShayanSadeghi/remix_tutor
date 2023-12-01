b0e3960a5af18b7651e97c5bfb25d76d9d9342ee
Shayan <ShayanSadeghi1996@gmail.com>
2023-12-01 19:02:15 +0330

    improve new contact action

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Form,
@@ -18,7 +18,7 @@ import { createEmptyContact, getContacts } from "./data";
 
 export const action = async () => {
   const contact = await createEmptyContact();
-  return json({ contact });
+  return redirect(`/contacts/${contact.id}/edit`);
 };
 
 export const loader = async () => {
